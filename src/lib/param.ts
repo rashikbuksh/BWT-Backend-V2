82c9dc57431f25e99c6859213a8dcef262e15453
rashikbuksh <rashikbuksh@gmail.com>
2025-07-14 00:25:40 +0600

    feat: update uuid validation to use zod schema

--- src/lib/param.ts
+++ src/lib/param.ts
@@ -1,9 +1,10 @@
-import getParamsSchema from 'stoker/openapi/schemas/get-params-schema';
 import SlugParamsSchema from 'stoker/openapi/schemas/slug-params';
+import { z } from 'zod';
 
-const uuid = getParamsSchema({
-  name: 'uuid',
-  validator: 'nanoid',
+const uuid = z.object({
+  uuid: z.string().length(15).regex(/^[0-9A-Z]{15}$/i, {
+    message: 'Invalid nanoid',
+  }),
 });
 
 const name = SlugParamsSchema;
