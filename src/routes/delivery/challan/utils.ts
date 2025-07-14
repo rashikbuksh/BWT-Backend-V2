d17339d00e19b286587a1df4703ee28804ecb814
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-07-15 02:41:03 +0600

    feat: add new route and handler for fetching challan details and entries by UUID

--- src/routes/delivery/challan/utils.ts
+++ src/routes/delivery/challan/utils.ts
@@ -11,6 +11,11 @@ export const insertSchema = createInsertSchema(
   challan,
   {
     uuid: schema => schema.uuid.length(15),
+    customer_uuid: schema => schema.customer_uuid.length(15),
+    employee_uuid: schema => schema.employee_uuid.length(15),
+    vehicle_uuid: schema => schema.vehicle_uuid.length(15),
+    courier_uuid: schema => schema.courier_uuid.length(15),
+    branch_uuid: schema => schema.branch_uuid.length(15),
     created_by: schema => schema.created_by.length(15),
     created_at: schema => schema.created_at.regex(dateTimePattern, {
       message: 'created_at must be in the format "YYYY-MM-DD HH:MM:SS"',
@@ -22,9 +27,17 @@ export const insertSchema = createInsertSchema(
   },
 ).required({
   uuid: true,
+  customer_uuid: true,
+  employee_uuid: true,
+  vehicle_uuid: true,
+  courier_uuid: true,
+  branch_uuid: true,
+  challan_type: true,
   created_by: true,
   created_at: true,
 }).partial({
+  is_delivery_complete: true,
+  payment_method: true,
   updated_at: true,
   remarks: true,
 });
