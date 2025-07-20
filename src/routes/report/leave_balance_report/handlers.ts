b4ee13a0e4e52835f25bf087df9b9bbb97c17ada
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-07-20 20:01:12 +0600

    refactor(report): rename leave history balance report to leave history report and update routes

--- src/routes/report/leave_balance_report/handlers.ts
+++ src/routes/report/leave_balance_report/handlers.ts
@@ -5,9 +5,9 @@ import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
 
-import type { LeaveHistoryBalanceReportRoute } from './routes';
+import type { LeaveBalanceReportRoute, LeaveHistoryReportRoute } from './routes';
 
-export const leaveHistoryBalanceReport: AppRouteHandler<LeaveHistoryBalanceReportRoute> = async (c: any) => {
+export const leaveHistoryReport: AppRouteHandler<LeaveHistoryReportRoute> = async (c: any) => {
   const { employee_uuid } = c.req.valid('query');
 
   const query = sql`
@@ -20,7 +20,42 @@ export const leaveHistoryBalanceReport: AppRouteHandler<LeaveHistoryBalanceRepor
         apply_leave.type as type,
         apply_leave.from_date as from_date,
         apply_leave.to_date as to_date,
-        apply_leave.reason
+        apply_leave.reason,
+        (apply_leave.to_date::date - apply_leave.from_date::date + 1) as days,
+        employment_type.name as employment_type_name
+    FROM
+        hr.apply_leave
+    LEFT JOIN
+        hr.employee ON employee.uuid = apply_leave.employee_uuid
+    LEFT JOIN
+        hr.users ON employee.user_uuid = users.uuid
+    LEFT JOIN
+        hr.leave_category ON apply_leave.leave_category_uuid = leave_category.uuid
+    LEFT JOIN 
+        hr.employment_type ON employee.employment_type_uuid = employment_type.uuid
+    WHERE 
+        ${employee_uuid ? sql`employee.uuid = ${employee_uuid}` : sql`TRUE`}
+  `;
+
+  const data = await db.execute(query);
+
+  return c.json(data.rows, HSCode.OK);
+};
+
+export const leaveBalanceReport: AppRouteHandler<LeaveBalanceReportRoute> = async (c: any) => {
+  const { employee_uuid } = c.req.valid('query');
+
+  const query = sql`
+    SELECT
+        employee.uuid as employee_uuid,
+        users.name as employee_name,
+        leave_category.uuid as leave_category_uuid,
+        leave_category.name as leave_category_name,
+        apply_leave.year as year,
+        apply_leave.type as type,
+        apply_leave.from_date as from_date,
+        apply_leave.to_date as to_date,
+        (DATE_PART('day', apply_leave.to_date - apply_leave.from_date) + 1) as days_count
     FROM
         hr.apply_leave
     LEFT JOIN
--- src/routes/report/leave_balance_report/index.ts
+++ src/routes/report/leave_balance_report/index.ts
@@ -4,6 +4,7 @@ import * as handlers from './handlers';
 import * as routes from './routes';
 
 const router = createRouter()
-  .openapi(routes.leaveHistoryBalanceReport, handlers.leaveHistoryBalanceReport);
+  .openapi(routes.leaveHistoryReport, handlers.leaveHistoryReport)
+  .openapi(routes.leaveBalanceReport, handlers.leaveBalanceReport);
 
 export default router;
