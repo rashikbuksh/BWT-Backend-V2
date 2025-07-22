9a983ce8592fab2c39d2bddaf105b6b4d1932d55
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-07-22 14:55:42 +0600

    fix: optimize product warehouse update logic in internal transfer functions

--- src/routes/trigger/product_after_internal_transfer.sql
+++ src/routes/trigger/product_after_internal_transfer.sql
@@ -1,4 +1,4 @@
--- inserted into database
+-- INSERT FUNCTION - Fixed to use single UPDATE
 CREATE OR REPLACE FUNCTION product_after_internal_transfer_insert_function() RETURNS TRIGGER AS $$
 DECLARE 
     from_warehouse_name TEXT;
@@ -7,44 +7,52 @@ BEGIN
     SELECT assigned INTO from_warehouse_name FROM store.warehouse WHERE uuid = NEW.from_warehouse_uuid;
     SELECT assigned INTO to_warehouse_name FROM store.warehouse WHERE uuid = NEW.to_warehouse_uuid;
     
+    -- Single UPDATE to handle both operations and prevent double processing
     UPDATE store.product
     SET
-        warehouse_1 = CASE WHEN from_warehouse_name = 'warehouse_1' THEN warehouse_1 - NEW.quantity ELSE warehouse_1 END,
-        warehouse_2 = CASE WHEN from_warehouse_name = 'warehouse_2' THEN warehouse_2 - NEW.quantity ELSE warehouse_2 END,
-        warehouse_3 = CASE WHEN from_warehouse_name = 'warehouse_3' THEN warehouse_3 - NEW.quantity ELSE warehouse_3 END,
-        warehouse_4 = CASE WHEN from_warehouse_name = 'warehouse_4' THEN warehouse_4 - NEW.quantity ELSE warehouse_4 END,
-        warehouse_5 = CASE WHEN from_warehouse_name = 'warehouse_5' THEN warehouse_5 - NEW.quantity ELSE warehouse_5 END,
-        warehouse_6 = CASE WHEN from_warehouse_name = 'warehouse_6' THEN warehouse_6 - NEW.quantity ELSE warehouse_6 END,
-        warehouse_7 = CASE WHEN from_warehouse_name = 'warehouse_7' THEN warehouse_7 - NEW.quantity ELSE warehouse_7 END,
-        warehouse_8 = CASE WHEN from_warehouse_name = 'warehouse_8' THEN warehouse_8 - NEW.quantity ELSE warehouse_8 END,
-        warehouse_9 = CASE WHEN from_warehouse_name = 'warehouse_7' THEN warehouse_9 - NEW.quantity ELSE warehouse_9 END,
-        warehouse_10 = CASE WHEN from_warehouse_name = 'warehouse_8' THEN warehouse_10 - NEW.quantity ELSE warehouse_10 END,
-        warehouse_11 = CASE WHEN from_warehouse_name = 'warehouse_9' THEN warehouse_11 - NEW.quantity ELSE warehouse_11 END,
-        warehouse_12 = CASE WHEN from_warehouse_name = 'warehouse_10' THEN warehouse_12 - NEW.quantity ELSE warehouse_12 END
-
-    WHERE uuid = NEW.product_uuid;
-    
-    UPDATE store.product
-    SET
-        warehouse_1 = CASE WHEN to_warehouse_name = 'warehouse_1' THEN warehouse_1 + NEW.quantity ELSE warehouse_1 END,
-        warehouse_2 = CASE WHEN to_warehouse_name = 'warehouse_2' THEN warehouse_2 + NEW.quantity ELSE warehouse_2 END,
-        warehouse_3 = CASE WHEN to_warehouse_name = 'warehouse_3' THEN warehouse_3 + NEW.quantity ELSE warehouse_3 END,
-        warehouse_4 = CASE WHEN to_warehouse_name = 'warehouse_4' THEN warehouse_4 + NEW.quantity ELSE warehouse_4 END,
-        warehouse_5 = CASE WHEN to_warehouse_name = 'warehouse_5' THEN warehouse_5 + NEW.quantity ELSE warehouse_5 END,
-        warehouse_6 = CASE WHEN to_warehouse_name = 'warehouse_6' THEN warehouse_6 + NEW.quantity ELSE warehouse_6 END,
-        warehouse_7 = CASE WHEN to_warehouse_name = 'warehouse_7' THEN warehouse_7 + NEW.quantity ELSE warehouse_7 END,
-        warehouse_8 = CASE WHEN to_warehouse_name = 'warehouse_8' THEN warehouse_8 + NEW.quantity ELSE warehouse_8 END,
-        warehouse_9 = CASE WHEN to_warehouse_name = 'warehouse_9' THEN warehouse_9 + NEW.quantity ELSE warehouse_9 END,
-        warehouse_10 = CASE WHEN to_warehouse_name = 'warehouse_10' THEN warehouse_10 + NEW.quantity ELSE warehouse_10 END,
-        warehouse_11 = CASE WHEN to_warehouse_name = 'warehouse_11' THEN warehouse_11 + NEW.quantity ELSE warehouse_11 END,
-        warehouse_12 = CASE WHEN to_warehouse_name = 'warehouse_12' THEN warehouse_12 + NEW.quantity ELSE warehouse_12 END
+        warehouse_1 = warehouse_1 
+            - CASE WHEN from_warehouse_name = 'warehouse_1' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_1' THEN NEW.quantity ELSE 0 END,
+        warehouse_2 = warehouse_2 
+            - CASE WHEN from_warehouse_name = 'warehouse_2' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_2' THEN NEW.quantity ELSE 0 END,
+        warehouse_3 = warehouse_3 
+            - CASE WHEN from_warehouse_name = 'warehouse_3' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_3' THEN NEW.quantity ELSE 0 END,
+        warehouse_4 = warehouse_4 
+            - CASE WHEN from_warehouse_name = 'warehouse_4' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_4' THEN NEW.quantity ELSE 0 END,
+        warehouse_5 = warehouse_5 
+            - CASE WHEN from_warehouse_name = 'warehouse_5' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_5' THEN NEW.quantity ELSE 0 END,
+        warehouse_6 = warehouse_6 
+            - CASE WHEN from_warehouse_name = 'warehouse_6' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_6' THEN NEW.quantity ELSE 0 END,
+        warehouse_7 = warehouse_7 
+            - CASE WHEN from_warehouse_name = 'warehouse_7' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_7' THEN NEW.quantity ELSE 0 END,
+        warehouse_8 = warehouse_8 
+            - CASE WHEN from_warehouse_name = 'warehouse_8' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_8' THEN NEW.quantity ELSE 0 END,
+        warehouse_9 = warehouse_9 
+            - CASE WHEN from_warehouse_name = 'warehouse_9' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_9' THEN NEW.quantity ELSE 0 END,
+        warehouse_10 = warehouse_10 
+            - CASE WHEN from_warehouse_name = 'warehouse_10' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_10' THEN NEW.quantity ELSE 0 END,
+        warehouse_11 = warehouse_11 
+            - CASE WHEN from_warehouse_name = 'warehouse_11' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_11' THEN NEW.quantity ELSE 0 END,
+        warehouse_12 = warehouse_12 
+            - CASE WHEN from_warehouse_name = 'warehouse_12' THEN NEW.quantity ELSE 0 END
+            + CASE WHEN to_warehouse_name = 'warehouse_12' THEN NEW.quantity ELSE 0 END
     WHERE uuid = NEW.product_uuid;
     
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
--- Trigger for delete
+-- DELETE FUNCTION - Fixed to use single UPDATE and fix warehouse_8 typo
 CREATE OR REPLACE FUNCTION product_after_internal_transfer_delete_function() RETURNS TRIGGER AS $$
 DECLARE 
     from_warehouse_name TEXT;
@@ -53,42 +61,50 @@ BEGIN
     SELECT assigned INTO from_warehouse_name FROM store.warehouse WHERE uuid = OLD.from_warehouse_uuid;
     SELECT assigned INTO to_warehouse_name FROM store.warehouse WHERE uuid = OLD.to_warehouse_uuid;
     
+    -- Single UPDATE to reverse the transfer
     UPDATE store.product
     SET
-        warehouse_1 = CASE WHEN from_warehouse_name = 'warehouse_1' THEN warehouse_1 + OLD.quantity ELSE warehouse_1 END,
-        warehouse_2 = CASE WHEN from_warehouse_name = 'warehouse_2' THEN warehouse_2 + OLD.quantity ELSE warehouse_2 END,
-        warehouse_3 = CASE WHEN from_warehouse_name = 'warehouse_3' THEN warehouse_3 + OLD.quantity ELSE warehouse_3 END,
-        warehouse_4 = CASE WHEN from_warehouse_name = 'warehouse_4' THEN warehouse_4 + OLD.quantity ELSE warehouse_4 END,
-        warehouse_5 = CASE WHEN from_warehouse_name = 'warehouse_5' THEN warehouse_5 + OLD.quantity ELSE warehouse_5 END,
-        warehouse_6 = CASE WHEN from_warehouse_name = 'warehouse_6' THEN warehouse_6 + OLD.quantity ELSE warehouse_6 END,
-        warehouse_7 = CASE WHEN from_warehouse_name = 'warehouse_7' THEN warehouse_7 + OLD.quantity ELSE warehouse_7 END,
-        warehouse_8 = CASE WHEN from_warehouse_name = 'warehouse_8' THEN warehouse_8 + OLD.quantity ELSE warehouse_8 END,
-        warehouse_9 = CASE WHEN from_warehouse_name = 'warehouse_9' THEN warehouse_9 + OLD.quantity ELSE warehouse_9 END,
-        warehouse_10 = CASE WHEN from_warehouse_name = 'warehouse_10' THEN warehouse_10 + OLD.quantity ELSE warehouse_10 END,
-        warehouse_11 = CASE WHEN from_warehouse_name = 'warehouse_11' THEN warehouse_11 + OLD.quantity ELSE warehouse_11 END,
-        warehouse_12 = CASE WHEN from_warehouse_name = 'warehouse_12' THEN warehouse_12 + OLD.quantity ELSE warehouse_12 END
-    WHERE uuid = OLD.product_uuid;
-    
-    UPDATE store.product
-    SET
-        warehouse_1 = CASE WHEN to_warehouse_name = 'warehouse_1' THEN warehouse_1 - OLD.quantity ELSE warehouse_1 END,
-        warehouse_2 = CASE WHEN to_warehouse_name = 'warehouse_2' THEN warehouse_2 - OLD.quantity ELSE warehouse_2 END,
-        warehouse_3 = CASE WHEN to_warehouse_name = 'warehouse_3' THEN warehouse_3 - OLD.quantity ELSE warehouse_3 END,
-        warehouse_4 = CASE WHEN to_warehouse_name = 'warehouse_4' THEN warehouse_4 - OLD.quantity ELSE warehouse_4 END,
-        warehouse_5 = CASE WHEN to_warehouse_name = 'warehouse_5' THEN warehouse_5 - OLD.quantity ELSE warehouse_5 END,
-        warehouse_6 = CASE WHEN to_warehouse_name = 'warehouse_6' THEN warehouse_6 - OLD.quantity ELSE warehouse_6 END,
-        warehouse_7 = CASE WHEN to_warehouse_name = 'warehouse_7' THEN warehouse_7 - OLD.quantity ELSE warehouse_7 END,
-        warehouse_8 = CASE WHEN to_warehouse_name = 'warehouse_8' THEN warehouse_9 - OLD.quantity ELSE warehouse_8 END,
-        warehouse_9 = CASE WHEN to_warehouse_name = 'warehouse_9' THEN warehouse_9 - OLD.quantity ELSE warehouse_9 END,
-        warehouse_10 = CASE WHEN to_warehouse_name = 'warehouse_10' THEN warehouse_10 - OLD.quantity ELSE warehouse_10 END,
-        warehouse_11 = CASE WHEN to_warehouse_name = 'warehouse_11' THEN warehouse_11 - OLD.quantity ELSE warehouse_11 END,
-        warehouse_12 = CASE WHEN to_warehouse_name = 'warehouse_12' THEN warehouse_12 - OLD.quantity ELSE warehouse_12 END
+        warehouse_1 = warehouse_1 
+            + CASE WHEN from_warehouse_name = 'warehouse_1' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_1' THEN OLD.quantity ELSE 0 END,
+        warehouse_2 = warehouse_2 
+            + CASE WHEN from_warehouse_name = 'warehouse_2' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_2' THEN OLD.quantity ELSE 0 END,
+        warehouse_3 = warehouse_3 
+            + CASE WHEN from_warehouse_name = 'warehouse_3' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_3' THEN OLD.quantity ELSE 0 END,
+        warehouse_4 = warehouse_4 
+            + CASE WHEN from_warehouse_name = 'warehouse_4' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_4' THEN OLD.quantity ELSE 0 END,
+        warehouse_5 = warehouse_5 
+            + CASE WHEN from_warehouse_name = 'warehouse_5' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_5' THEN OLD.quantity ELSE 0 END,
+        warehouse_6 = warehouse_6 
+            + CASE WHEN from_warehouse_name = 'warehouse_6' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_6' THEN OLD.quantity ELSE 0 END,
+        warehouse_7 = warehouse_7 
+            + CASE WHEN from_warehouse_name = 'warehouse_7' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_7' THEN OLD.quantity ELSE 0 END,
+        warehouse_8 = warehouse_8 
+            + CASE WHEN from_warehouse_name = 'warehouse_8' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_8' THEN OLD.quantity ELSE 0 END,
+        warehouse_9 = warehouse_9 
+            + CASE WHEN from_warehouse_name = 'warehouse_9' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_9' THEN OLD.quantity ELSE 0 END,
+        warehouse_10 = warehouse_10 
+            + CASE WHEN from_warehouse_name = 'warehouse_10' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_10' THEN OLD.quantity ELSE 0 END,
+        warehouse_11 = warehouse_11 
+            + CASE WHEN from_warehouse_name = 'warehouse_11' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_11' THEN OLD.quantity ELSE 0 END,
+        warehouse_12 = warehouse_12 
+            + CASE WHEN from_warehouse_name = 'warehouse_12' THEN OLD.quantity ELSE 0 END
+            - CASE WHEN to_warehouse_name = 'warehouse_12' THEN OLD.quantity ELSE 0 END
     WHERE uuid = OLD.product_uuid;
     
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
-
 -- Trigger for update
 
 CREATE OR REPLACE FUNCTION product_after_internal_transfer_update_function() RETURNS TRIGGER AS $$
