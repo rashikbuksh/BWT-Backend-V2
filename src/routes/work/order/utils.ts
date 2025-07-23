779ed47a8f1d20fdeceb3ef31c0d43039458bee1
rashikbuksh <rashikbuksh@gmail.com>
2025-07-23 13:36:39 +0600

    work/order -> schema issue fixed

--- src/routes/work/order/utils.ts
+++ src/routes/work/order/utils.ts
@@ -1,23 +1,30 @@
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
 
 import { dateTimePattern } from '@/utils';
 
-import { info } from '../schema';
+import { order } from '../schema';
 
 //* crud
-export const selectSchema = createSelectSchema(info);
+export const selectSchema = createSelectSchema(order);
 
 export const insertSchema = createInsertSchema(
-  info,
+  order,
   {
     uuid: schema => schema.uuid.length(15),
-    user_uuid: schema => schema.user_uuid.length(15),
-    received_date: schema => schema.received_date.regex(dateTimePattern, {
-      message: 'received_date must be in the format "YYYY-MM-DD HH:MM:SS"',
-    }),
-    zone_uuid: schema => schema.zone_uuid.length(15),
+    info_uuid: schema => schema.info_uuid.length(15),
+    model_uuid: schema => schema.model_uuid.length(15).optional(),
+    serial_no: schema => schema.serial_no.optional(),
+    quantity: z.number().positive().default(0),
+    problems_uuid: schema => schema.problems_uuid.optional(),
+    problem_statement: schema => schema.problem_statement,
+    accessories: schema => schema.accessories.optional(),
+    is_diagnosis_need: schema => schema.is_diagnosis_need.default(false),
+    warehouse_uuid: schema => schema.warehouse_uuid.length(15).optional(),
+    rack_uuid: schema => schema.rack_uuid.length(15).optional(),
+    floor_uuid: schema => schema.floor_uuid.length(15).optional(),
+    box_uuid: schema => schema.box_uuid.length(15).optional(),
     created_by: schema => schema.created_by.length(15),
-
     created_at: schema => schema.created_at.regex(dateTimePattern, {
       message: 'created_at must be in the format "YYYY-MM-DD HH:MM:SS"',
     }),
@@ -25,21 +32,60 @@ export const insertSchema = createInsertSchema(
       message: 'updated_at must be in the format "YYYY-MM-DD HH:MM:SS"',
     }),
     remarks: schema => schema.remarks.optional(),
+    is_transferred_for_qc: schema => schema.is_transferred_for_qc.default(false),
+    is_ready_for_delivery: schema => schema.is_ready_for_delivery.default(false),
+    brand_uuid: schema => schema.brand_uuid.length(15).optional(),
+    is_proceed_to_repair: schema => schema.is_proceed_to_repair.default(false),
+    repairing_problems_uuid: schema => schema.repairing_problems_uuid.optional(),
+    qc_problems_uuid: schema => schema.qc_problems_uuid.optional(),
+    delivery_problems_uuid: schema => schema.delivery_problems_uuid.optional(),
+    repairing_problem_statement: schema => schema.repairing_problem_statement.optional(),
+    qc_problem_statement: schema => schema.qc_problem_statement.optional(),
+    delivery_problem_statement: schema => schema.delivery_problem_statement.optional(),
+    ready_for_delivery_date: schema => schema.updated_at.regex(dateTimePattern, {
+      message: 'ready_for_delivery_date must be in the format "YYYY-MM-DD HH:MM:SS"',
+    }).optional(),
+    bill_amount: z.number().default(0),
+    is_home_repair: schema => schema.is_home_repair.default(false),
+    proposed_cost: z.number().default(0),
+    is_challan_needed: schema => schema.is_challan_needed.default(false),
   },
 ).required({
   uuid: true,
-  id: true,
-  user_uuid: true,
-  zone_uuid: true,
+  info_uuid: true,
+  problem_statement: true,
   created_by: true,
   created_at: true,
 }).partial({
-  received_date: true,
-  is_product_received: true,
-  location: true,
-  submitted_by: true,
+  model_uuid: true,
+  serial_no: true,
+  quantity: true,
+  problems_uuid: true,
+  accessories: true,
+  is_diagnosis_need: true,
+  warehouse_uuid: true,
+  rack_uuid: true,
+  floor_uuid: true,
+  box_uuid: true,
   updated_at: true,
   remarks: true,
+  is_transferred_for_qc: true,
+  is_ready_for_delivery: true,
+  brand_uuid: true,
+  is_proceed_to_repair: true,
+  repairing_problems_uuid: true,
+  qc_problems_uuid: true,
+  delivery_problems_uuid: true,
+  repairing_problem_statement: true,
+  qc_problem_statement: true,
+  delivery_problem_statement: true,
+  ready_for_delivery_date: true,
+  bill_amount: true,
+  is_home_repair: true,
+  proposed_cost: true,
+  is_challan_needed: true,
+}).omit({
+  id: true,
 });
 
 export const patchSchema = insertSchema.partial();
