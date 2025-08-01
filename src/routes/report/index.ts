f5b1d4e1695c944617d20a5ed16fc1874d42c28f
Rashik Buksh <rashikbuksh@gmail.com>
2025-08-01 20:05:02 +0600

    refactor: replace leave_balance_report with leave_report and reorganize report routes

--- src/routes/report/index.ts
+++ src/routes/report/index.ts
@@ -1,9 +1,11 @@
+import absent_report from './absent_report';
 import attendance_report from './attendance_report';
-import leave_balance_report from './leave_balance_report';
+import leave_report from './leave_report';
 
 const report = [
-  leave_balance_report,
+  leave_report,
   attendance_report,
+  absent_report,
 ];
 
 export default report;
