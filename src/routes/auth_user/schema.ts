775a0e927c0a7dbaab951c59233e0d1a7e6bc437
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-08-28 18:00:45 +0600

    fix: change user ID type from serial to text in user schema

--- src/routes/auth_user/schema.ts
+++ src/routes/auth_user/schema.ts
@@ -1,9 +1,9 @@
-import { boolean, pgSchema, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { boolean, pgSchema, text, timestamp } from 'drizzle-orm/pg-core';
 
 const auth_user = pgSchema('auth_user');
 
 export const user = auth_user.table('user', {
-  id: serial('id').primaryKey(),
+  id: text('id').primaryKey(),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
   emailVerified: boolean('email_verified')
