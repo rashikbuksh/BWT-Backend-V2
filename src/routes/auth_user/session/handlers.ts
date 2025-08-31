b6c630fb898ec93690d66397079dd8dfa9a7a675
rashikbuksh <rashikbuksh@gmail.com>
2025-08-31 18:35:55 +0600

    fix: refactor session retrieval in list handler to use auth API for improved efficiency

--- src/routes/auth_user/session/handlers.ts
+++ src/routes/auth_user/session/handlers.ts
@@ -2,29 +2,16 @@ import type { AppRouteHandler } from '@/lib/types';
 
 import * as HSCode from 'stoker/http-status-codes';
 
-import db from '@/db';
+import { auth } from '@/lib/auth';
 
 import type {
   ListRoute,
 } from './routes';
 
-import { session } from '../schema';
-
 export const list: AppRouteHandler<ListRoute> = async (c: any) => {
-  const sessionPromise = db
-    .select({
-      id: session.id,
-      expiresAt: session.expiresAt,
-      token: session.token,
-      createdAt: session.createdAt,
-      updatedAt: session.updatedAt,
-      ipAddress: session.ipAddress,
-      userAgent: session.userAgent,
-      userId: session.userId,
-    })
-    .from(session);
+  const session = await auth.api.getSession({
+    headers: c.req.raw.headers,
+  });
 
-  const data = await sessionPromise;
-
-  return c.json(data || [], HSCode.OK);
+  return c.json(session || [], HSCode.OK);
 };
