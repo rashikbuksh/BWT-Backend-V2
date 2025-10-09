9156afe0fdc77e83a6a6e413492aacb8c8028bac
Rashik Buksh Rafsan <rashikbuksh@gmail.com>
2025-10-10 01:21:04 +0600

    refactor(zkteco): remove unused ZK routes and handlers

--- src/routes/other/index.ts
+++ src/routes/other/index.ts
@@ -1,16 +1,13 @@
 import delivery from './delivery';
-// import delivery from './delivery';
 import hr from './hr';
 import store from './store';
 import work from './work';
-import zkteco from './zkteco';
 
 const other = [
   ...hr,
   ...store,
   ...work,
   ...delivery,
-  zkteco,
 ];
 
 export default other;
