a0ec0c1cf4925265901229e5a198aeb440766801
rashikbuksh <rashikbuksh@gmail.com>
2025-10-26 15:07:27 +0600

    refactor: add is_return field to order details in list response

--- src/routes/work/info/handlers.ts
+++ src/routes/work/info/handlers.ts
@@ -273,6 +273,7 @@ export const list: AppRouteHandler<ListRoute> = async (c: any) => {
                     'model_name', m.name,
                     'brand_uuid', m.brand_uuid,
                     'brand_name', b.name,
+                    'is_return', o.is_return,
                     'is_diagnosis_need', o.is_diagnosis_need,
                     'is_proceed_to_repair', o.is_proceed_to_repair,
                     'is_transferred_for_qc', o.is_transferred_for_qc,
