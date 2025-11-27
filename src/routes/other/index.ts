27eaf0d00fe06c3362c020b5617c39a9f0c3a49b
rashikbuksh <rashikbuksh@gmail.com>
2025-11-27 15:22:32 +0600

    fix: include inventory routes in the other routes aggregation

--- src/routes/other/index.ts
+++ src/routes/other/index.ts
@@ -1,3 +1,4 @@
+import inventory from '../inventory';
 import delivery from './delivery';
 import hr from './hr';
 import store from './store';
@@ -8,6 +9,7 @@ const other = [
   ...store,
   ...work,
   ...delivery,
+  ...inventory,
 ];
 
 export default other;
