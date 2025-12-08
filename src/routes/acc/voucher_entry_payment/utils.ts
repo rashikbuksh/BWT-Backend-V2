d4a4fa27dfcf98dfb15b512cf4828da931e6e80f
Rashik Buksh Rafsan <rashikbuksh@gmail.com>
2025-12-08 12:57:56 +0600

    feat: update insertSchema and selectSchema to use voucher_entry_payment

--- src/routes/acc/voucher_entry_payment/utils.ts
+++ src/routes/acc/voucher_entry_payment/utils.ts
@@ -3,22 +3,22 @@ import z from 'zod';
 
 import { dateTimePattern } from '@/utils';
 
-import { voucher_entry } from '../schema';
+import { voucher_entry_payment } from '../schema';
 
 //* crud
-export const selectSchema = createSelectSchema(voucher_entry);
+export const selectSchema = createSelectSchema(voucher_entry_payment);
 
 export const insertSchema = createInsertSchema(
-  voucher_entry,
+  voucher_entry_payment,
   {
     uuid: schema => schema.uuid.length(15),
-    ledger_uuid: schema => schema.ledger_uuid.length(15),
-    type: schema => schema.type.default('dr'),
     amount: z.number().default(0),
-    is_need_cost_center: schema => schema.is_need_cost_center.default(false),
-    is_payment: schema => schema.is_payment.default(false),
-    description: schema => schema.description.optional(),
-    voucher_uuid: schema => schema.voucher_uuid.length(15),
+    index: z.number(),
+    date: schema => schema.date.regex(dateTimePattern, {
+      message: 'date must be in the format "YYYY-MM-DD HH:MM:SS"',
+    }).optional(),
+    voucher_entry_uuid: schema => schema.voucher_entry_uuid.length(15),
+    trx_no: schema => schema.trx_no.optional(),
     created_by: schema => schema.created_by.length(15),
     created_at: schema => schema.created_at.regex(dateTimePattern, {
       message: 'created_at must be in the format "YYYY-MM-DD HH:MM:SS"',
@@ -30,16 +30,14 @@ export const insertSchema = createInsertSchema(
   },
 ).required({
   uuid: true,
-  ledger_uuid: true,
-  type: true,
-  voucher_uuid: true,
+  index: true,
+  voucher_entry_uuid: true,
   created_by: true,
   created_at: true,
 }).partial({
+  trx_no: true,
+  date: true,
   amount: true,
-  is_need_cost_center: true,
-  is_payment: true,
-  description: true,
   updated_by: true,
   updated_at: true,
   remarks: true,
